5019548385c3114d71c3dcc08ade7543eac6322f
patt26 <kharchepratik123@gmail.com>
2021-05-06 21:00:41 +0530

    Added Queue and Stack

---
+++ DataStructure/LinkedListQueue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class LinkedListQueue
+    {
+        Node head = null;
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into Queue", node.data);
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal void Dequeue()
+        {
+            if (this.head == null)
+            {
+                Console.WriteLine("Queue is empty,Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0}", this.head.data);
+            this.head = this.head.next;
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -10,14 +10,33 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            var linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
-            linkedList.Add(70);
-            linkedList.Display();
-           // linkedList.InsertAtPerticularPosition(2, 30);
-           // linkedList.RemoveFirstNode();
+            //var linkedList = new LinkedList();
+            //linkedList.Add(56);
+            //linkedList.Add(30);
+            //linkedList.Add(70);
+            //linkedList.Display();
+            //// linkedList.InsertAtPerticularPosition(2, 30);
+            //// linkedList.RemoveFirstNode();
+
+
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(30);
+            stack.Push(50);
+            stack.Push(70);
+            stack.Pop();
+            stack.Display();
+            stack.Peek();
+            stack.IsEmpty();
+            stack.Display();
+
+            //LinkedListQueue queue = new LinkedListQueue();
+            //queue.Enqueue(56);
+            //queue.Enqueue(30);
+            //queue.Enqueue(80);
+            //queue.Dequeue();
+            //queue.Display();
             Console.ReadKey();
+            
         }
     }
 }
